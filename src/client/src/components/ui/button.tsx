59eb7f491818c41e988ccc4c0a3a62bc9d7d63af
KL0maboi <shakshatho@gmail.com>
2026-09-19 16:21:06 +0530

    added empty frontend vite project with tailwind & shadcn initalized

---
+++ src/client/src/components/ui/button.tsx
@@ -0,0 +1,20 @@
+import { Button as ButtonPrimitive } from '@base-ui/react/button';
+import { cn } from 'cn';
+import { type ButtonVariants, buttonVariants } from './button-variants';
+
+function Button({
+    className,
+    variant = 'default',
+    size = 'default',
+    ...props
+}: ButtonPrimitive.Props & ButtonVariants) {
+    return (
+        <ButtonPrimitive
+            data-slot="button"
+            className={cn(buttonVariants({ variant, size, className }))}
+            {...props}
+        />
+    );
+}
+
+export { Button };
